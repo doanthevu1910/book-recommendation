d406e2d91cc844b128669caf84c9681533abeac2
doanthevu1910 <doanthevu1910@gmail.com>
2022-02-27 20:17:42 +0100

    Added clean.py

--- clean.py
+++ clean.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 
 books = pd.read_csv('data/books.csv',
@@ -20,4 +21,25 @@ books.dtypes
 books = books.dropna()
 books.isnull().sum()
 
+ratings = pd.read_csv('data/ratings.csv',
+                    sep=';',
+                    error_bad_lines=False,
+                    encoding='latin-1')
+
+ratings.head()
+ratings = ratings.dropna()
+ratings.columns = ['ID', 'ISBN', 'rating']
+
+users = pd.read_csv('data/users.csv',
+                    sep=';',
+                    error_bad_lines=False,
+                    encoding='latin-1')
+
+users = users.dropna()
+users.columns = ['ID', 'location', 'age']
+
+users.loc[(users.age > 100) | (users.age < 5), 'age'] = users.age.mean()
+sorted(users.age.unique())
+users.age = users.age.astype('int32')
+users.dtypes
 
